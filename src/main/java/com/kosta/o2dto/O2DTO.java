33b4bd111a4e5c1223243896c1e37e6b394a7d9b
choisy <beast12511@gmail.com>
2021-09-18 14:45:24 +0900

    dto 추가

---
+++ src/main/java/com/kosta/o2dto/O2DTO.java
@@ -0,0 +1,16 @@
+package com.kosta.o2dto;
+
+import javax.annotation.Resource;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter @Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class O2DTO {
+
+}
