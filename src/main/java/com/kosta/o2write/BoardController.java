30ecfac09129779161348b3b1d6c8463775599b8
anko <gantri300@gmail.com>
2021-09-21 22:02:09 +0900

    페이징 및 검색처리

--- src/main/java/com/kosta/o2write/BoardController.java
+++ src/main/java/com/kosta/o2write/BoardController.java
@@ -1,8 +1,31 @@
 package com.kosta.o2write;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kosta.o2service.O2Service;
 
 @Controller
 public class BoardController {
+	@Autowired
+	O2Service service;
 	
+	@RequestMapping("/main")
+	public String list(@RequestParam(required = false, defaultValue = "1") int currPage
+	           ,@RequestParam(required = false, defaultValue = "") String search
+	           ,@RequestParam(required = false, defaultValue = "") String searchtxt
+	           ,Model model) {
+		
+		
+		
+		
+	
+		
+		return "list";
+	}
 }
