687fb22ec085de8f3558f4a355a4dee04f78efe2
shshnn <ssss5489@naver.com>
2021-09-27 23:28:53 +0900

    user 추가

--- src/main/java/com/kosta/o2controller/UserController.java
+++ src/main/java/com/kosta/o2controller/UserController.java
@@ -1,23 +1,57 @@
 package com.kosta.o2controller;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kosta.o2dto.O2UserDTO;
 import com.kosta.o2service.O2UserService;
 
 @Controller
+@RequestMapping("/member/*")
 public class UserController {
 
-	@Autowired
+	private static final Logger logger=LoggerFactory.getLogger(UserController.class);
+	
+	@Inject
 	O2UserService service;
 	  
-	
-	@GetMapping("/login")
+	//login
+	@RequestMapping("/login")
 	public String login() {
 		return "member/login";
 	}
-	
+	//login 처리
+	@RequestMapping("loginCheck")
+	public ModelAndView login(@ModelAttribute O2UserDTO dto,HttpSession session) {
+		boolean result= service.login(dto, session);
+		ModelAndView mav=new ModelAndView();
+		if(result==true) {
+			//main
+			mav.setViewName("main");
+			mav.addObject("msg","success");
+			
+		}else {
+			mav.setViewName("member/login");
+			mav.addObject("msg","failure");
+		}
+		return mav;
+	}
+	//logout
+	@RequestMapping("logout")
+	public ModelAndView logout(HttpSession session) {
+		service.logout(session);
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("member/login");
+		mav.addObject("msg","logout");
+		return mav;
+	}
 }
---
+++ src/main/webapp/WEB-INF/views/member/login.jsp
@@ -0,0 +1,62 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib  prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<%@ include file="../include/header.jsp" %>
+<script>
+$(document).ready(function(){
+	$("#btnLogin").click(function(){
+		var user_id= $("#user_id").val();
+		var pwd= $("#pwd").val();
+		if(user_id == ""){
+			alert("아이디를 입력하세요");
+			$("#user_id").focus();//입력포커스 이동
+			return; //함수 종료
+		}
+		if(pwd == ""){
+			alert("비밀번호를 입력하세요");
+			$("#pwd").focus();
+			return;
+		}
+		document.form1.action=${path}/member/loginCheck"
+		document.form1.submit();
+	});
+});
+</script>
+</head>
+<body>
+<h2>로그인</h2>
+<form name="form1" method="post">
+<table border="1" width="400px">
+<tr>
+	<td>아이디</td>
+	<td><input type="text" name="user_id" id="user_id"></td>
+</tr>
+<tr>
+	<td>비밀번호</td>
+	<td><input type="password" name="pwd" id="pwd"></td>
+</tr>
+<tr>
+	<td colspan="2" align="center">
+		<button type="button" id="btnLogin">로그인</button>
+	<c:if test="${msg == 'failure' }">
+		<div style="color: red">
+			아이디 또는 비밀번호가 일치하지 않습니다
+		</div>
+	</c:if>
+	<c:if test="${msg == 'logout' }">
+		<div style="color: red">
+			로그아웃되었습니다.
+		</div>
+	</c:if>
+	</td>
+</tr>
+</table>
+</form>
+</body>
+</html>
