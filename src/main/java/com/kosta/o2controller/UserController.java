11320d36f9eda742f2776318a7bf0e2027ba70dd
shshnn <ssss5489@naver.com>
2021-09-27 14:54:36 +0900

    Usercontroller

--- src/main/java/com/kosta/o2controller/UserController.java
+++ src/main/java/com/kosta/o2controller/UserController.java
@@ -1,8 +1,19 @@
 package com.kosta.o2controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kosta.o2dto.O2UserDTO;
 
 @Controller
 public class UserController {
 
+	@RequestMapping("login")
+	public String login() {
+		return "member/login";
+	}
 }
