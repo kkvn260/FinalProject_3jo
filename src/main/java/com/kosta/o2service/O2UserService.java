5c4c064a2cab351c83336ca90dc539f67c97e8b8
anko <gantri300@gmail.com>
2021-09-27 15:37:25 +0900

    로그인 틀

--- src/main/java/com/kosta/o2service/O2UserService.java
+++ src/main/java/com/kosta/o2service/O2UserService.java
@@ -1,5 +1,15 @@
 package com.kosta.o2service;
 
+import javax.servlet.http.HttpSession;
+
+import com.kosta.o2dto.O2UserDTO;
+
 public interface O2UserService {
   
+	boolean login(O2UserDTO dto, HttpSession session);
+	
+	O2UserDTO UserInfo(O2UserDTO dto);
+	
+	void logout(HttpSession session); 
+	
 }
--- src/main/java/com/kosta/o2service/O2UserServiceImple.java
+++ src/main/java/com/kosta/o2service/O2UserServiceImple.java
@@ -1,8 +1,37 @@
 package com.kosta.o2service;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kosta.o2dao.O2UserDAO;
+import com.kosta.o2dto.O2UserDTO;
 
 @Service
 public class O2UserServiceImple implements O2UserService {
 
+	@Autowired
+	private O2UserDAO dao;
+	
+	@Override
+	public boolean login(O2UserDTO dto, HttpSession session) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public O2UserDTO UserInfo(O2UserDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void logout(HttpSession session) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 } 
