fe93e6b0d3d15af1eec53318834e6dda72db3f1a
anko <gantri300@gmail.com>
2021-10-01 14:50:56 +0900

    userService 수정

--- src/main/java/com/kosta/o2service/O2UserService.java
+++ src/main/java/com/kosta/o2service/O2UserService.java
@@ -15,15 +15,15 @@ import com.kosta.o2dto.O2UserDTO;
 @Service
 public class O2UserService {
   
+	
 	@Autowired
-	private SqlSessionTemplate sqlSession;
 	private O2UserDAO userdao;
 	
 	//회원가입
 	public int signUser(O2UserDTO userdto) {
 		int resultcount =0;
 		
-		userdao= sqlSession.getMapper(O2UserDAO.class);
+		
 		try {
 			resultcount = userdao.signUser(userdto);
 			System.out.println("dto 확인" +userdto.getUser_id()); 
@@ -36,13 +36,13 @@ public class O2UserService {
 	// 아이디 중복 체크
 	public int userIdCheck(String user_id) {
 		// TODO Auto-generated method stub
-		userdao = sqlSession.getMapper(O2UserDAO.class);
+		
 		return userdao.checkUserId(user_id);
 	}
 
 	public int login(O2UserDTO userdto) throws Exception {
 		// TODO Auto-generated method stub
-		userdao= sqlSession.getMapper(O2UserDAO.class);
+		
 		return userdao.login(userdto);
 		
 	}
